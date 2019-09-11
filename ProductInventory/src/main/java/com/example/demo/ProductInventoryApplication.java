4b27c45da12b5a3dad748c51c17dff9a1bfde702
vinay <krvinay68@gmail.com>
2019-09-12 05:03:48 +0530

    updated new logic

--- ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
+++ ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
@@ -3,13 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.example" })
 public class ProductInventoryApplication {
 
+	@Bean
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductInventoryApplication.class, args);
 	}
--- ProductInventory/src/main/java/com/example/services/Products.java
+++ ProductInventory/src/main/java/com/example/services/Products.java
@@ -1,7 +1,7 @@
 package com.example.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.models.ProductDetails;
+import com.example.models.ProductInfo;
+import com.example.models.UserReviewList;
 
 @RestController
 @RequestMapping("/productservice")
@@ -21,6 +23,12 @@ public class Products {
 	@RequestMapping("/{userId}") 
 	public List<ProductDetails> getProductDetails(@PathVariable("userId") String userId) {
 		
-		return null;
+     UserReviewList reviews = restTemplate.getForObject("http://localhost:8083/reviewservice/"+userId, UserReviewList.class);
+     
+     return reviews.getUserReview().stream().map(review -> {
+			ProductInfo productInfo= restTemplate.getForObject("http://localhost:8081/productinfoservice/"+review.getProductId(), ProductInfo.class);
+			return new ProductDetails(productInfo.getProductId(),"demo",review.getUserRating());
+		 }).collect(Collectors.toList());	
+		
 	}
 }
