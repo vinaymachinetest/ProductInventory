3754cf3429b22ee26ec6d975c859d9e3f8a861c6
vinay <krvinay68@gmail.com>
2019-09-12 05:20:34 +0530

    added new api

---
+++ ProductInformation/src/main/java/com/example/services/ProductInfoService.java
@@ -0,0 +1,17 @@
+package com.example.services;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.models.ProductInfo;
+
+@RestController
+@RequestMapping("/productinfoservice")
+public class ProductInfoService {
+
+	@RequestMapping("/{productId}") 
+	public ProductInfo getProductInfo(@PathVariable("productId") String productId) {
+		return new ProductInfo(productId, "HRX Shoes");
+	}	
+	
+}
