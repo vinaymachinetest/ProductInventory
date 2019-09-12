d18f8ef147e9061555ff2a9dcb381eeae56fba6c
vinay <krvinay68@gmail.com>
2019-09-12 06:46:01 +0530

    updated api call

--- ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
+++ ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class ProductInventoryApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getTemplate() {
 		return new RestTemplate();
 	}
