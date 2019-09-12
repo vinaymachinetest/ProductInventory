f8bd70e82aaac2f63f890e731bebcc2b8942240f
vinay <krvinay68@gmail.com>
2019-09-12 06:31:26 +0530

    enabled eureka client

--- ProductUserReview/src/main/java/com/example/demo/ProductUserReviewApplication.java
+++ ProductUserReview/src/main/java/com/example/demo/ProductUserReviewApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.example" })
+@EnableEurekaClient
 public class ProductUserReviewApplication {
 
 	public static void main(String[] args) {
