258dd46177f5f909491bd0e6a68acdd615c4e4bc
vinay <krvinay68@gmail.com>
2019-09-12 06:28:23 +0530

    enabled eureka client

--- ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
+++ ProductInventory/src/main/java/com/example/demo/ProductInventoryApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.example" })
+@EnableEurekaClient
 public class ProductInventoryApplication {
 
 	@Bean
